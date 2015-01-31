7c4bfbb500ef48b45fda458e79e60abafe348e91
Sina Sadeghi <sina.sa@gmail.com>
2015-01-31 06:08:44 +0000

    OpenSSL CA scripts
    Generate CA key/cert pair with genCA and generate server certs
    to be signed by the CA key with genServerCerts.

---
+++ CA/genCA.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Create CA private key
+openssl genrsa -aes256 -out cakey.pem 4096
+
+# Create CA self-signed certificate
+openssl req -sha256 -new -x509 -extensions v3_ca -days 999 -key cakey.pem -out cacert.pem
---
+++ CA/genServerCerts.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+node=$1
+if ! [ $node ]; then
+  echo node not set.
+  exit 1
+fi
+
+if ! [ -d $node ]; then
+  mkdir $node
+  openssl genrsa -aes256 -out $node/${node}key.pem 4096
+  openssl req -sha256 -new -days 999 -key $node/${node}key.pem -out $node/${node}csr.pem
+  openssl x509 -req -in $node/${node}csr.pem -CA cacert.pem -CAkey cakey.pem -CAcreateserial -out $node/${node}cert.pem -days 999
+else
+  echo "$node already exists."
+fi
