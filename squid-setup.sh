21a72b93d1ca8ee5a66f9cf2f94ca4dcbcf23345
Sina Sadeghi <sina.sa@gmail.com>
2015-01-31 06:04:33 +0000

    squid setup
    setup a squid caching server inside the appropriate jail

---
+++ squid-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -x
+
+JAILHOME=/home
+JAILNAME=squid
+
+echo "nameserver 8.8.4.4" > ${JAILHOME}/${JAILNAME}/etc/resolv.conf
+
+pkg -j $JAILNAME remove squid
+pkg -j $JAILNAME autoremove 
+
+echo y | pkg -j $JAILNAME install squid
+
+echo "squid_enable=\"YES\"" > ${JAILHOME}/${JAILNAME}/etc/rc.conf
+
+echo "nameserver 192.168.254.11
+search int.sina.id.au" > ${JAILHOME}/${JAILNAME}/etc/resolv.conf
+
+cp -f config/common/FreeBSD.conf ${JAILHOME}/${JAILNAME}/etc/pkg/FreeBSD.conf
+
+echo "192.168.254.10 squid" >> ${JAILHOME}/${JAILNAME}/etc/hosts
+
+jexec $JAILNAME service squid restart
+
+sed -i .bk '/^192.168.254.10 squid/d' ${JAILHOME}/${JAILNAME}/etc/hosts
