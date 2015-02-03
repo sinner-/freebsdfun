33b67476dd8da0eade116cb74981ce2794cbb42a
Sina Sadeghi <sina.sa@gmail.com>
2015-02-03 07:28:38 +0000

    add permissions hardening to genCA and genServerCerts scripts

--- CA/genCA.sh
+++ CA/genCA.sh
@@ -4,3 +4,7 @@ openssl genrsa -aes256 -out cakey.pem 4096
 
 # Create CA self-signed certificate
 openssl req -sha256 -new -x509 -extensions v3_ca -days 999 -key cakey.pem -out cacert.pem
+
+#Set secure permissions
+chmod og-rwx cakey.pem
+chmod og-rwx cacert.pem
--- CA/genServerCerts.sh
+++ CA/genServerCerts.sh
@@ -11,6 +11,7 @@ if ! [ -d $node ]; then
   openssl genrsa -aes256 -out $node/${node}key.pem 4096
   openssl req -sha256 -new -days 999 -key $node/${node}key.pem -out $node/${node}csr.pem
   openssl x509 -req -in $node/${node}csr.pem -CA cacert.pem -CAkey cakey.pem -CAcreateserial -out $node/${node}cert.pem -days 999
+  chmod -R og-rwx $node
 else
   echo "$node already exists."
 fi
