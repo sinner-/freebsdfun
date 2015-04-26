184326fe8ca97093937ef75e0687d067c0ccde9c
Sina Sadeghi <sina.sa@gmail.com>
2015-04-26 06:46:22 +0000

    openvpn setup
    add configuration to turn FreeBSD fun node into an OpenVPN gateway.
    
    whitelist vpn subnet for DNS lookups
    setup NAT for hosts coming over the tun
    secure SSH to be only over the VPN
    add script to generate DH file for PFS
    OpenVPN client-client comms enabled

---
+++ CA/genDH.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+openssl dhparam -out dh2048.pem 2048
---
+++ openvpn-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -x
+
+JAILHOME=/home
+JAILNAME=openvpn
+
+export http_proxy="http://squid:3128/"
+
+echo y | pkg install openvpn
+
+mkdir /etc/rc.conf.d
+cp -f config/master/openvpn.rc.conf /etc/rc.conf.d/openvpn
+sysctl net.inet.ip.forwarding=1
+
+mkdir /usr/local/etc/openvpn
+cp -f config/master/openvpn.conf /usr/local/etc/openvpn/openvpn.conf
+
+mkdir /usr/local/etc/ssl
+mkdir /usr/local/etc/ssl/private
+
+cp -f CA/openvpn/openvpnkey.nocrypt.pem /usr/local/etc/ssl/private/server.key
+cp -f CA/openvpn/openvpncert.pem /usr/local/etc/ssl/server.crt
+cp -f CA/cacert.pem /usr/local/etc/ssl/ca.crt
+cp -f CA/openvpn/dh2048.pem /usr/local/etc/ssl/dh2048.pem
+
+chmod -R og-rwx /usr/local/etc/ssl/private
+
+service openvpn restart
